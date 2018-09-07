9490b50c1d98930f4773865b9ae29719a0024826
Kyrylo Stanchyk <exbifour@gmail.com>
2018-09-07 17:09:42 +0300

    Updated authorization - Product edition now require authorization - Product management now moved to dedicated controller

---
+++ CoffeeCircles/Controllers/AdminController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeCircles.Data;
+using CoffeeCircles.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CoffeeCircles.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _env;
+
+        public AdminController(ApplicationDbContext db, IHostingEnvironment env)
+        {
+            _db = db;
+            _env = env;
+        }
+
+        public IActionResult AddProduct()
+        {
+            return RedirectToAction("EditProduct", new Product());
+        }
+
+        private List<SelectListItem> CollectProductTypes()
+        {
+            return _db.ProductTypes
+                   .Select(pt => new SelectListItem()
+                   {
+                       Value = pt.ProductTypeId.ToString(),
+                       Text = pt.Name
+                   })
+                   .ToList();
+        }
+
+        [HttpGet]
+        public IActionResult EditProduct(Product product)
+        {
+            ViewBag.ProductTypes = CollectProductTypes();
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewProductAsync(IFormFile photo, Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+
+                if (photo == null)
+                {
+                    product.PhotoRef = "/Images/no-image.png";
+                }
+                else
+                {
+                    string path = "/Images/Products/"
+                        + product.ProductId
+                        + photo.FileName.Substring(photo.FileName.LastIndexOf('.'));
+                    using (FileStream fs = new FileStream(_env.WebRootPath + path, FileMode.Create))
+                    {
+                        await photo.CopyToAsync(fs);
+                    }
+                    product.PhotoRef = path;
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Products", "Home");
+            }
+
+            ViewBag.ProductTypes = CollectProductTypes();
+            return RedirectToAction("EditProduct", product);
+        }
+    }
+}
--- CoffeeCircles/Controllers/HomeController.cs
+++ CoffeeCircles/Controllers/HomeController.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoffeeCircles.Models;
 using Microsoft.AspNetCore.Http;
-using System.IO;
 using CoffeeCircles.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeCircles.Controllers
@@ -17,12 +15,10 @@ namespace CoffeeCircles.Controllers
     public class HomeController : Controller
     {
         readonly ApplicationDbContext _db;
-        private readonly IHostingEnvironment _env;
 
-        public HomeController(ApplicationDbContext db, IHostingEnvironment env)
+        public HomeController(ApplicationDbContext db)
         {
             _db = db;
-            _env = env;
         }
 
         public IActionResult Index()
@@ -36,57 +32,6 @@ namespace CoffeeCircles.Controllers
             return View();
         }
 
-        [HttpGet]
-        public IActionResult EditProduct()
-        {
-            ViewBag.ProductTypes = _db.ProductTypes
-                   .Select(pt => new SelectListItem()
-                   {
-                       Value = pt.ProductTypeId.ToString(),
-                       Text = pt.Name
-                   })
-                   .ToList();
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddNewProduct(IFormFile photo, Product product)
-        {
-            if(ModelState.IsValid)
-            {
-                _db.Products.Add(product);
-                await _db.SaveChangesAsync();
-
-                if (photo == null)
-                {
-                    product.PhotoRef = "/Images/no-image.png";
-                }
-                else
-                {
-                    string path = "/Images/Products/"
-                        + product.ProductId
-                        + photo.FileName.Substring(photo.FileName.LastIndexOf('.'));
-                    using (FileStream fs = new FileStream(_env.WebRootPath + path, FileMode.Create))
-                    {
-                        await photo.CopyToAsync(fs);
-                    }
-                    product.PhotoRef = path;
-                }
-
-                await _db.SaveChangesAsync();
-
-                return RedirectToAction("Products");
-            }
-
-            ViewBag.ProductTypes = _db.ProductTypes
-                   .Select(pt => new SelectListItem()
-                   {
-                       Value = pt.ProductTypeId.ToString(),
-                       Text = pt.Name
-                   })
-                   .ToList();
-            return View("EditProduct");
-        }
 
         public IActionResult Privacy()
         {
